e5eacb3c738f3640ad88e5f095e86cb3c96acb00
Ryan Murphy <TheDecoder007@gmail.com>
2022-05-30 16:01:57 -0400

    changed PORT to work with heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { animals } = require('./data/animals.json');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 //route that front-end cant request data from. 
@@ -62,6 +63,6 @@ app.get('/api/animals', (req, res) => {
 });
 
 //sets up server on a port (3001)
-app.listen(3001, () => {
-    console.log(`API server now on port 3001`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
 });
